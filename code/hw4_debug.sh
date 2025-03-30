f69d1c9370260334b315dc1ca69b25c2b42d0d27
Hozen <enzo.durel@gmail.com>
2025-03-30 16:04:32 -0500

    add debug sh file

---
+++ code/hw4_debug.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#SBATCH --partition=debug_gpu
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=30G
+#SBATCH --output=results/debug_%j_stdout.txt
+#SBATCH --error=results/debug_%j_stderr.txt
+#SBATCH --time=00:30:00
+#SBATCH --job-name=hw4_debug
+#SBATCH --mail-user=Enzo.B.Durel-1@ou.edu
+#SBATCH --mail-type=ALL
+#SBATCH --chdir=/home/cs504305/hw4/code/
+#SBATCH --array=0
+
+#################################################
+# Do not change this line unless you have your own python/tensorflow/keras set up
+. /home/fagg/tf_setup.sh
+conda activate dnn
+
+## SHALLOW
+python hw4.py @exp.txt @oscer.txt @net_shallow.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK --epochs 50 -vvv
