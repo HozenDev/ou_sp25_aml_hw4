f8c40cee4893ad3446fdd84e7bc32a23e74bf48b
Hozen <enzo.durel@gmail.com>
2025-03-31 00:13:35 -0500

    debug

--- code/hw4_deep.sh
+++ code/hw4_deep.sh
@@ -7,7 +7,7 @@
 #SBATCH --output=results/deep_%j_stdout.txt
 #SBATCH --error=results/deep_%j_stderr.txt
 #SBATCH --time=02:00:00
-#SBATCH --job-name=hw3_deep
+#SBATCH --job-name=hw4_deep
 #SBATCH --mail-user=Enzo.B.Durel-1@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504305/hw4/code/
@@ -19,4 +19,4 @@
 conda activate dnn
 
 ## SHALLOW
-python hw4.py @exp.txt @oscer.txt @net_deep.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK --epochs 3000 -vvv --save_model --render
+python hw4.py -vvv @exp.txt @oscer.txt @net_deep.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK --save_model --render --cache ""
--- code/hw4_shallow.sh
+++ code/hw4_shallow.sh
@@ -7,7 +7,7 @@
 #SBATCH --output=results/shallow_%j_stdout.txt
 #SBATCH --error=results/shallow_%j_stderr.txt
 #SBATCH --time=02:00:00
-#SBATCH --job-name=hw3_shallow
+#SBATCH --job-name=hw4_shallow
 #SBATCH --mail-user=Enzo.B.Durel-1@ou.edu
 #SBATCH --mail-type=ALL
 #SBATCH --chdir=/home/cs504305/hw4/code/
@@ -19,4 +19,4 @@
 conda activate dnn
 
 ## SHALLOW
-python hw4.py -v @exp.txt @oscer.txt @net_shallow.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK -vvv --save_model --render --cache ""
+python hw4.py -vvv @exp.txt @oscer.txt @net_shallow.txt --exp_index $SLURM_ARRAY_TASK_ID --cpus_per_task $SLURM_CPUS_PER_TASK --save_model --render --cache ""
